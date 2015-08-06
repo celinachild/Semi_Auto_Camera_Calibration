1e7d257b1c76c20297f65ab7a231d711286bd4ae
DongwonShin <dongwonshin@gist.ac.kr>
2015-08-06 15:52:05 +0900

    non-maximum supression

--- main.py
+++ main.py
@@ -5,9 +5,15 @@ import cv2
 from matplotlib import pyplot as plt
 import math
 
-param_FAST = 30
-rad_circular_sampling = 10
+from logging import FileHandler
+from vlogging import VisualRecord
+import logging
+
+# Global Parameters
 cam_num = 4
+seq_num = 10
+param_FAST = 20
+rad_circular_sampling = 10
 
 def circle_check1(img_thresh, _kp, rad):
 
@@ -60,16 +66,44 @@ def circular_sampling(img, kp, rad):
         if circle_check1(img_thresh, _kp, rad) or circle_check2(img_thresh, _kp, rad):
             pattern_features.append(_kp)
 
-    #print pattern_features
+    def distance(a,b):
+        ax = a.pt[0]
+        ay = a.pt[1]
+        bx = b.pt[0]
+        by = b.pt[1]
 
-    for pf in pattern_features:
-        cv2.circle(img, (int(pf.pt[0]), int(pf.pt[1])), 5, (0,0,255))
+        return math.sqrt((ax-bx)**2 + (ay-by)**2)
+
+    # non-maximum supression
+    new_pattern_features = []
+    for pf1 in pattern_features:
+        flag = True
+        for pf2 in new_pattern_features:
+            if distance(pf1, pf2) < 100:
+                flag = False
+                break
+
+        if flag:
+            new_pattern_features.append(pf1)
+
+    # print pattern_features
+    for pf in new_pattern_features:
+        cv2.circle(img, (int(pf.pt[0]), int(pf.pt[1])), 5, (0,0,255), 3)
 
     cv2.imshow("pattern_features", img)
 
+    return new_pattern_features
+
 
 # main
-for i in range(1,10):
+logger = logging.getLogger("demo")
+fh = FileHandler('log.html',mode="w")
+logger.setLevel(logging.DEBUG)
+logger.addHandler(fh)
+
+#logger.debug(VisualRecord("img_corners", img_corners,"image corners", fmt="png"))
+
+for i in range(1,seq_num+1):
     filename = "seq/20150805/cam%d/IND000%d%02d.bmp" % (cam_num+1, cam_num ,i)
 
     img = cv2.imread(filename)
@@ -81,9 +115,8 @@ for i in range(1,10):
     # find and draw the keypoints
     kp = fast.detect(img,None)
     img_corners = cv2.drawKeypoints(img, kp, color=(255,0,0))
-    cv2.imshow("img_corners", img_corners)
+    #cv2.imshow("img_corners", img_corners)
 
     circular_sampling(img, kp, rad_circular_sampling)
-
 
     cv2.waitKey()
